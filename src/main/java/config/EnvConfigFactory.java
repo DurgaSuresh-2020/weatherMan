5403145fe0d8ce9c2097f218a6454009c7ee34fb
Durga Suresh <durgavs2020@gmail.com>
2023-02-17 23:03:28 -0800

    Added config factory

---
+++ src/main/java/config/EnvConfigFactory.java
@@ -0,0 +1,18 @@
+package config;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+//@Slf4j
+// For future reference on this topic: https://github.com/lightbend/config
+public class EnvConfigFactory {
+    // Load default properties (first from System properties and then from application.conf)
+    private static final Config BASE_CONFIG = ConfigFactory.load();
+    private static final String HOST = BASE_CONFIG.getString("HOST");
+    private static final Config HOST_CONFIG = ConfigFactory.load(HOST);
+    private static final Config MERGED_CONFIG = HOST_CONFIG.withFallback(BASE_CONFIG);
+
+    public static final Config getConfig() {
+        return MERGED_CONFIG;
+    }
+}
