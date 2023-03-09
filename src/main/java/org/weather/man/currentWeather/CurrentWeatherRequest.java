8e02a82c56cab7367cb39a35b622a9c6c2183e74
Durga Suresh <durgavs2020@gmail.com>
2023-03-08 22:45:13 -0800

    supporting entities for current weather api and basic test

--- src/main/java/org/weather/man/currentWeather/CurrentWeatherRequest.java
+++ src/main/java/org/weather/man/currentWeather/CurrentWeatherRequest.java
@@ -4,11 +4,14 @@ package org.weather.man.currentWeather;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 
+import static org.weather.man.config.EnvConfigFactory.getConfig;
+
 public class CurrentWeatherRequest {
-    public static RequestSpecification createLocationsRequest() {
+    public static RequestSpecification createWeatherRequest() {
         return RestAssured
                 .given()
                 .spec(CurrentWeatherSpec.getBasicSpec())
+                .queryParam("key",getConfig().getString("API_KEY"))
                 .log().ifValidationFails();
     }
 }
---
+++ src/test/java/org/weather/man/currentWeather/currentWeather/TestCurrentWeather.java
@@ -0,0 +1,20 @@
+package org.weather.man.currentWeather.currentWeather;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.weather.man.currentWeather.CurrentWeatherAPI;
+
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.weather.man.currentWeather.assertResponse.AssertResponse.assertResponseStatusIs;
+
+public class TestCurrentWeather{
+    @Nested
+    class Get {
+        @Test
+        void assertThatCurrentWeatherIsReturnedForValidCityName() {
+            Response response = CurrentWeatherAPI.getCurrentWeather("Amsterdam");
+            assertResponseStatusIs(SC_OK,response);
+        }
+    }
+}
